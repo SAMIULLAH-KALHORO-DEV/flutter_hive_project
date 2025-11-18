e5c9db1125524fd7b123905849da53e79cec0615
Samiullah Kalhoro <91752628+SAMIULLAH-KALHORO-DEV@users.noreply.github.com>
2025-11-19 00:28:28 +0500

    very defficult

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,40 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Hive dase')),
+      appBar: AppBar(title: Text('Hive database')),
       body: Column(
         children: [
           FutureBuilder(
             future: Hive.openBox('kashif'),
             builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-                return Column(
-                  children: [
-                    Text(snapshot.data!.get('name').toString()),
-                    Text(snapshot.data!.get('age').toString()),
-                  
-                  ],
-                );
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
               }
-              return CircularProgressIndicator(); // Or any loading indicator
+              return Column(
+                children: [
+                  Text(snapshot.data!.get('name').toString()),
+                  Text(snapshot.data!.get('age').toString()),
+                  Text(snapshot.data!.get('details').toString()),
+                ],
+              );
+            },
+          ),
+          FutureBuilder(
+            future: Hive.openBox('names'),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return Column(children: [Text(snapshot.data!.get('youtube').toString())]);
             },
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
+          var box2 = await Hive.openBox('names');
           var box = await Hive.openBox('kashif');
 
-          box.put('name', 'kashif');
-          box.put('age', 22);
-          // print(box.get('name'));
-          // print(box.get('age'));
+          box2.put('youtube', 'randomshorts');
+          box.put('name', 'sami');
+          box.put('details', {'name': 'kashif', 'phone': '030'});
+          box.put('age', '2');
+          print(box.get('name'));
+          print(box.get('details')['phone']);
         },
         child: Icon(Icons.add),
       ),
